434988e92a1742e962f745819ce5e149a35603a1
2skydev <2skydev@gmail.com>
2024-07-11 14:35:57 +0900

    fix(base-url): 환경 변수 수정

--- src/features/connect/modules/getBaseUrl.ts
+++ src/features/connect/modules/getBaseUrl.ts
@@ -1,4 +1,5 @@
 export const getBaseUrl = () => {
-  if (process.env.NEXT_PUBLIC_VERCEL_URL) return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  if (process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL)
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
   return `http://localhost:${process.env.PORT ?? 3000}`
 }
